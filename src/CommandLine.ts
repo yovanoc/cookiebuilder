f6b0ec318d3b92a3e8f66d9c8ec3957342d266d5
Christopher Yovanovitch <yovano_c@outlook.com>
2018-06-01 21:59:49 +0200

    cleaning

--- src/CommandLine.ts
+++ src/CommandLine.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import * as yargs from 'yargs';
 import * as fs from 'fs';
+import * as yargs from 'yargs';
 
 import { SwfReader } from 'xswf';
 import { ITagDoAbc, TagCode } from 'xswf/dist/Types';
 import { extractD2Enums } from './D2EnumExtractor';
 
-yargs
+const args = yargs
   .usage('Usage: $0 --src [filePath] --out [filePath]')
   .demandOption(['src', 'out'])
   .alias('src', 's')
@@ -15,16 +15,16 @@ yargs
   .describe('src', 'Path to the DofusInvoker.swf')
   .describe('out', 'Where to save the generated protocol')
   .epilog('Copyright © 2018 DevChris & Aegis')
-  .argv
+  .argv;
 
-const reader = new SwfReader(yargs.argv.src);
+const reader = new SwfReader(args.src);
 const file = reader.getFile();
 const doAbc = file.tags.find((tag) => tag.code === TagCode.DoABC) as ITagDoAbc;
 const abcFile = doAbc.abcFile;
 
 fs.writeFileSync(
-  yargs.argv.out,
-  JSON.stringify(extractD2Enums(abcFile), null, 2)
+  args.out,
+  JSON.stringify(extractD2Enums(abcFile), null, 2),
 );
 
 console.log('Protocol generated!');
--- src/D2EnumExtractor.ts
+++ src/D2EnumExtractor.ts
@@ -1,19 +1,19 @@
-import { IQName, MultinameKind } from 'xswf/dist/abcFile/types/multiname';
-import { IInstanceInfo } from 'xswf/dist/abcFile/types/instance';
-import { TraitKind } from 'xswf/dist/abcFile/types/trait';
-import { ConstantKind } from 'xswf/dist/abcFile/types/constant';
 import { IAbcFile } from 'xswf/dist/abcFile/types';
+import { ConstantKind } from 'xswf/dist/abcFile/types/constant';
+import { IInstanceInfo } from 'xswf/dist/abcFile/types/instance';
+import { IQName, MultinameKind } from 'xswf/dist/abcFile/types/multiname';
+import { TraitKind } from 'xswf/dist/abcFile/types/trait';
 
-export type D2EnumEntry = {
-  name: string,
-  value: number,
-};
+export interface ID2EnumEntry {
+  name: string;
+  value: number;
+}
 
-export type D2Enum = {
-  package: string,
-  name: string,
-  entries: D2EnumEntry[],
-};
+export interface ID2Enum {
+  package: string;
+  name: string;
+  entries: ID2EnumEntry[];
+}
 
 export function extractD2Enum(klass: IInstanceInfo) {
   if (
@@ -23,13 +23,13 @@ export function extractD2Enum(klass: IInstanceInfo) {
     throw new Error('invalid input class');
   }
 
-  let d2Enum: D2Enum = {
+  const d2Enum: ID2Enum = {
     package: klass.name.ns.name,
     name: klass.name.name,
-    entries: []
+    entries: [],
   };
 
-  klass.class.traits.forEach(trait => {
+  klass.class.traits.forEach((trait) => {
     if (trait.kind !== TraitKind.Const) {
       return;
     }
@@ -48,10 +48,9 @@ export function extractD2Enum(klass: IInstanceInfo) {
   return d2Enum;
 }
 
-
-export function extractD2Enums(abcFile: IAbcFile): D2Enum[] {
+export function extractD2Enums(abcFile: IAbcFile): ID2Enum[] {
   return abcFile.instances.filter(
-    c => c.name.kind === MultinameKind.QName &&
-    c.name.ns.name.startsWith('com.ankamagames.dofus.network.enums')
+    (c) => c.name.kind === MultinameKind.QName &&
+    c.name.ns.name.startsWith('com.ankamagames.dofus.network.enums'),
   ).map(extractD2Enum);
 }
